a26900d0a741460906a52539bd9ad20d244e2661
Alex Bormotov <alex.bormotov.work@gmail.com>
2019-11-11 21:15:19 +0300

    Hello, World!

---
+++ precache-manifest.434077c5a1ef8e4880df515d79d294c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f8b5ced188d79488f1c7452913cbc85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b497d5b75e45f7829bb",
+    "url": "/static/css/main.e3c89dba.chunk.css"
+  },
+  {
+    "revision": "7f285bd0d81c129b4ffe",
+    "url": "/static/js/2.ba2a9284.chunk.js"
+  },
+  {
+    "revision": "9b497d5b75e45f7829bb",
+    "url": "/static/js/main.bed322ef.chunk.js"
+  },
+  {
+    "revision": "42a7d95d5a60ad8d7303",
+    "url": "/static/js/runtime-main.c7888e06.js"
+  },
+  {
+    "revision": "f5048e9bbd13ff196aa2a149d5103059",
+    "url": "/static/media/logo.f5048e9b.svg"
+  }
+]);
